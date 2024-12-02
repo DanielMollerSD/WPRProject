6e01c94a25bb9d7ea316ada3e5fd01627942183d
ayberkyonar <yonarayberk@gmail.com>
2024-12-02 15:26:58 +0100

    frontend: component footer begin gemaakt met background color

--- frontend/src/components/footer/index.jsx
+++ frontend/src/components/footer/index.jsx
@@ -4,10 +4,42 @@ function Footer() {
 
     return (
         <>
-            <div>
-                <h1>Footer</h1>
+        <div className="component component-footer">
+        <footer className='footer'>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-md-4'>
+                        <h4>edlldww</h4>
+                        <ul>
+                            <li>dewqdq</li>
+                            <li>dewqdq</li>
+                            <li>dewqdq</li>
+                            <li>dewqdq</li>
+                        </ul>
+                    </div>
+                    <div className='col-md-4'>
+                        <h4>hewcs</h4>
+                        <ul>
+                            <li>dewqdq</li>
+                            <li>dewqdq</li>
+                            <li>dewqdq</li>
+                            <li>dewqdq</li>
+                        </ul>
+                    </div>
+                    <div className='col-md-4'>
+                        <h4>test</h4>
+                        <ul>
+                            <li>dewqdq</li>
+                            <li>dewqdq</li>
+                            <li>dewqdq</li>
+                            <li>dewqdq</li>
+                        </ul>
+                    </div>
+                </div>
             </div>
-        </>
+        </footer>
+        </div>
+        </> 
     )
 }
 
