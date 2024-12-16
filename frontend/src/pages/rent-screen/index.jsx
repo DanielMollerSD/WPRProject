9d48c78774043978708925f7e89e1e713a2f0b2a
DanielMollerSD <daniel.moller2003@gmail.com>
2024-12-16 14:12:08 +0100

    Frontend: Vehicle-overview styling improved

--- frontend/src/pages/rent-screen/index.jsx
+++ frontend/src/pages/rent-screen/index.jsx
@@ -9,7 +9,7 @@ function RentScreen() {
     useEffect(() => {
         async function fetchVehicles() {
             try {
-                const response = await fetch(`${'https://localhost:7265/api'}/Vehicle`);
+                const response = await fetch(`https://localhost:7265/api/Vehicle`);
                 if (!response.ok) throw new Error("Failed to fetch vehicles");
                 console.log(response)
                 
@@ -44,18 +44,18 @@ function RentScreen() {
                                 <div className="col-md-6 col-lg-4" key={index}>
                                     <a href="#">
                                         <div className="card">
-                                            <img className="card-img-top" src="" alt="Card image" />
+                                            <img className="card-img-top" src="https://via.placeholder.com/500x300" alt="Card image" />
                                             <div className="card-body">
                                                 <h5 className="card-title">{vehicle.brand} {vehicle.model}</h5>
-                                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                                <div className="icons">
-                                                    <div className="icon"><strong>Type:</strong> {vehicle.vehicleType}</div>
-                                                    <div className="icon"><strong>Merk:</strong> {vehicle.brand}</div>
-                                                    <div className="icon"><strong>Model:</strong> {vehicle.model}</div>
-                                                    <div className="icon"><strong>Koop Jaar:</strong> {vehicle.purchaseYear}</div>
-                                                    <div className="icon"><strong>Status:</strong> {vehicle.status}</div>
+                                                <p className="card-description">{vehicle.note}</p>
+                                                <div className="tags">
+                                                    <div className="tag"><strong>Type:</strong> {vehicle.vehicleType}</div>
+                                                    <div className="tag"><strong>Merk:</strong> {vehicle.brand}</div>
+                                                    <div className="tag"><strong>Model:</strong> {vehicle.model}</div>
+                                                    <div className="tag"><strong>Koop Jaar:</strong> {vehicle.purchaseYear}</div>
+                                                    <div className="tag"><strong>Status:</strong> {vehicle.status}</div>
                                                 </div>
-                                                <div className="price"><strong>Prijs:</strong> €{vehicle.price}</div>
+                                                <div className="price">€{vehicle.price} <span> /dag</span></div>
                                             </div>
                                         </div>
                                     </a>
