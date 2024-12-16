07f4aae81585f68648cd2eaa21aca6c5690d2490
ayberkyonar <yonarayberk@gmail.com>
2024-12-16 11:56:16 +0100

    foto + tekst homepage

--- frontend/src/pages/home/index.jsx
+++ frontend/src/pages/home/index.jsx
@@ -1,10 +1,27 @@
-
+import './styles.scss'
 
 function Home() {
 
     return (
         <>
-            <div><h1>Homepage!</h1></div>
+        <div className="page page-home">
+            <div class="container mt-5">
+                <div class="row">
+                    <div class="col-md-6">
+                        <img src="https://via.placeholder.com/400x200" class="card-img-top" alt="Card Image"></img>
+                    </div>
+                    <div class="col-md-6 d-flex align-items-center">
+                        <div>
+                            <h2>Lorem Ipsum</h2>
+                            <p>Lorem ipsum dolor sit amet. Est rerum autem ut provident nihil vel omnis minus et sint exercitationem ut debitis natus eum optio incidunt.</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque interdum nulla id nisi vehicula, a fermentum sapien posuere.</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque interdum nulla id nisi vehicula, a fermentum sapien posuere.</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque interdum nulla id nisi vehicula, a fermentum sapien posuere.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         </>
     )
 }
