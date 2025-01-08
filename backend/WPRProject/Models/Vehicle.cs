93af10456b262c03e27d0a52906b1cca2803064a
Your Denvey <302283510@student.rocmondriaan.nl>
2025-01-08 21:27:38 +0100

    frontend en backend crud vehicle

--- backend/WPRProject/Models/Vehicle.cs
+++ backend/WPRProject/Models/Vehicle.cs
@@ -4,28 +4,29 @@ using System.Text.Json.Serialization;
 namespace WPRProject.Tables
 {
     public class Vehicle
-    {
-        [Key] public int Id { get; set; }
-      
-        public string LicensePlate { get; set; }
-   
-        public string Brand { get; set; }
-       
-        public string Model { get; set; }
-       
-        public string Color { get; set; }
-        public string Status { get; set; }
-        public string Note { get; set; }
-      
-        public double Price {  get; set; }
+{
+    public int Id { get; set; }
 
-        [StringLength(4, MinimumLength = 4, ErrorMessage ="Voer een geldig jaar getal in")]
-        public int PurchaseYear {  get; set; }
-        public string VehicleType {  get; set; }
+    [Required]
+    public string LicensePlate { get; set; }
 
-        [JsonIgnore]
-        // Navigation property to link rents (one-to-many)
-        public ICollection<Rent> Rents { get; set; }
+    [Required]
+    public string Brand { get; set; }
 
-    }
+    [Required]
+    public string Model { get; set; }
+
+    public string Color { get; set; }
+    public string Status { get; set; }
+    public string Note { get; set; }
+
+    [Range(1, int.MaxValue, ErrorMessage = "Prijs moet een positief nummer zijn.")]
+    public decimal Price { get; set; }
+
+    [Range(1900, 2100, ErrorMessage = "Voer een geldig jaar in.")]
+    public int PurchaseYear { get; set; }
+
+    [Required]
+    public string VehicleType { get; set; }
+}
 }
