2312e70dba3870a6a1973ba20c6b9fc4a8c69c4c
Alex-xyc <Zhang_xin_yuan@hotmail.com>
2025-01-26 16:03:55 +0100

    home page styling

--- frontend/src/pages/home/index.jsx
+++ frontend/src/pages/home/index.jsx
@@ -8,34 +8,26 @@ function Home() {
           <div className="row">
             <div className="col-md-6">
               <img
-                src="https://via.placeholder.com/400x200"
+                src="https://img.freepik.com/vrije-photo/een-tevreden-klant-in-een-autoshowroom-die-een-goede-deal-maakt_1303-17385.jpg?semt=ais_hybrid"
                 className="card-img-top"
                 alt="Card Image"
               ></img>
             </div>
             <div className="col-md-6 d-flex align-items-center">
               <div>
-                <h2>Lorem Ipsum</h2>
-              
+                <h2>"Beste auto huur site van Nederland"</h2>
+
                 <p>
-                  Lorem ipsum dolor sit amet. Est rerum autem ut provident nihil
-                  vel omnis minus et sint exercitationem ut debitis natus eum
-                  optio incidunt.
+                  Welkom bij Cars And All, uw vertrouwde partner voor het huren
+                  van voertuigen! Bij ons kunt u terecht voor een breed scala
+                  aan voertuigen, van auto's tot campers en caravans, allemaal
+                  zorgvuldig geselecteerd om aan uw behoeften te voldoen.
                 </p>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Quisque interdum nulla id nisi vehicula, a fermentum sapien
-                  posuere.
-                </p>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Quisque interdum nulla id nisi vehicula, a fermentum sapien
-                  posuere.
-                </p>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Quisque interdum nulla id nisi vehicula, a fermentum sapien
-                  posuere.
+                  Bij Cars And All streven we ernaar om niet alleen uw
+                  verwachtingen te vervullen, maar ze te overtreffen. We staan
+                  voor kwaliteit, betrouwbaarheid, en service die u kunt
+                  vertrouwen!
                 </p>
               </div>
             </div>
